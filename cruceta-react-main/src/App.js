583e89d6012e32a92f904f357cf90b1e0d6625ad
Llanez <95553215+Llanez@users.noreply.github.com>
2022-05-07 20:18:32 -0700

    Add files via upload

---
+++ cruceta-react-main/src/App.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+import './style.css';
+
+function App() {
+  return (
+    <><div className="margen">
+      <div className="capa c4">
+        <div className="esquina-superior">
+          <div className="cubito blue"></div>
+          <div className="cubito blue"></div>
+        </div>
+
+        <div className="esquina-inferior">
+          <div className="cubito blue"></div>
+          <div className="cubito blue"></div>
+        </div>
+      </div>
+    </div><div className="margen-border">
+
+      </div><div className="capa c3">
+        <div className="esquina-superior">
+          <div className="cubito cream"></div>
+          <div className="cubito cream"></div>
+        </div>
+
+        <div className="esquina-inferior">
+          <div className="cubito cream"></div>
+          <div className="cubito cream"></div>
+        </div>
+      </div><div className="capa c2">
+        <div className="esquina-superior">
+          <div className="cubito red"></div>
+          <div className="cubito red"></div>
+        </div>
+
+        <div className="esquina-inferior">
+          <div className="cubito red"></div>
+          <div className="cubito red"></div>
+        </div>
+      </div><div class="cuadro-central purple">
+        <div class="mitad1 orange"></div>
+        <div class="mitad2 orangered"></div>
+      </div><div class="circulo-central blue">
+        <div class="puntero"></div>
+      </div><div class="capa c1">
+        <div class="esquina-superior">
+          <div class="cubito green"></div>
+          <div class="cubito green"></div>
+        </div>
+
+        <div class="esquina-inferior">
+          <div class="cubito green"></div>
+          <div class="cubito green"></div>
+        </div>
+      </div></>
+  );
+}
+
+export default App;
